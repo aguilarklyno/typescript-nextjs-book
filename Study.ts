ac30afc9d4e39a758ad4f00c8b47e1fbf85b87cd
aguilarklyno <aguilarklyno@gmail.com>
2023-02-10 01:13:13 +0900

    2.2.5

--- Study.ts
+++ Study.ts
@@ -22,5 +22,23 @@ mynumber: "にひゃく"
 
 const array: string[] = []
 array.push("Takuya")
-// array.push(1)
+// array.push(1) // 配列の方と合わないためエラーになる
+
+const mixArray = ["foo", 1]
+const mixArrayU: (string | number)[] = ["foo", 1] //union型
+const mixArrayT: [string, number] = ["foo", 1] //タプル
+
+// string型のnameとnumber型のageのみを持つオブジェクトの型を定義
+const user: { name: string; age: number } = {
+  name: "Takeru",
+  age: 36
+}
+
+console.log(user.name)
+console.log(user.age)
+
+function peintName(obj: { firstName: string; lastName?: string }) {
+  
+}
+
 
