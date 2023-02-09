e94e7d857bc8483656c85387b7a5c494e2bfbfd8
aguilarklyno <aguilarklyno@gmail.com>
2023-02-10 01:02:48 +0900

    2.2.3

---
+++ Study.ts
@@ -0,0 +1,26 @@
+let employeeName : string = "John"
+
+function calc(isSum: boolean) {
+  let a = 100
+  if (isSum) {
+    let b = a + 1
+    return b
+  }
+  // return b
+}
+
+const num: number = 100
+// num = 100
+
+let age: number = 36
+let isDone: boolean = false
+let color: string = "青"
+
+let mynumber: string = "200"
+mynumber: "にひゃく"
+// mynumber = 200
+
+const array: string[] = []
+array.push("Takuya")
+// array.push(1)
+
